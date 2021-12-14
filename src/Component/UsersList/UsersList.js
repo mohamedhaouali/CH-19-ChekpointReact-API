a4233d8d073fc6f1c328460bda6ed2b63e51f760
mohamedhaouali <mohamed.haouali1@gmail.com>
2021-12-14 09:43:51 +0100

    react-api

---
+++ src/Component/UsersList/UsersList.js
@@ -0,0 +1,37 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import User from "../User/User";
+import "./UsersList.css";
+
+const UsersList = () => {
+
+  // state
+
+  const [Data, setData] = useState([]);
+
+  // recuperer le fichier json
+
+  const GetData = () => {
+    axios
+      .get("https://jsonplaceholder.typicode.com/users")
+
+      .then((res) => {
+        setData(res.data);
+      })
+      .catch((error) => console.log(`Error : ${error}`));
+  };
+
+  useEffect(() => {
+    GetData();
+  });
+
+  return (
+    <div className="Users">
+      {Data.map((el) => (
+        <User key={el.id} user={el} />
+      ))}
+    </div>
+  );
+};
+
+export default UsersList;
